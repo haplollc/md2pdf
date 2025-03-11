8c59c9941619f0924f3ab72d990ebcfcdaf883c3
jaredcassoutt <jaredcassoutt@gmail.com>
2025-03-11 12:26:14 -0700

    Initial Commit

---
+++ md2pdf/md2pdfApp.swift
@@ -0,0 +1,17 @@
+//
+//  md2pdfApp.swift
+//  md2pdf
+//
+//  Created by Jared Cassoutt on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct md2pdfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ md2pdfTests/md2pdfTests.swift
@@ -0,0 +1,17 @@
+//
+//  md2pdfTests.swift
+//  md2pdfTests
+//
+//  Created by Jared Cassoutt on 3/11/25.
+//
+
+import Testing
+@testable import md2pdf
+
+struct md2pdfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
